dca265ec5d0887e560b09501a554d2f5f83292ae
srujana-porandla <131435996+srujana-porandla@users.noreply.github.com>
2024-06-25 16:08:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+document.getElementById('btn').addEventListener('click', async()=>{
+	const text=document.getElementById('text').value;
+	const delay = parseInt(document.getElementById('delay').value, 10);
+
+	 const outputDiv = document.getElementById('output');
+     outputDiv.innerHTML = '';
+	function delayFor(ms) {
+		return new Promise(resolve=>setTimeout(resolve, ms));
+	}
+	async function displayMessage(message,ms) {
+		await delayFor(ms);
+		outputDiv.textContent=message;
+		
+	}
+	displayMessage(text,delay);
+})
